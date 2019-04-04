14f3d48350f390e88898a12d1ace49f76086636d
Guenter Hummel <guenter.hummel@sbs.co.at>
2019-04-04 14:29:50 +0200

    Moved to GitHub

---
+++ hailstorm.py
@@ -0,0 +1,44 @@
+# calculate collatz sequence for a user supplied number
+
+# print the sequence nicely, print the sequence size. 
+
+start_str = input("Working with what number : ")
+start_int = int(start_str)
+
+count = 1
+seq_int = start_int
+print("Here is the Collatz sequence starting at : ",start_str)
+
+print('{0:5d}, '.format(seq_int), end='')
+while seq_int > 1:
+
+    # collatz calculation
+    if seq_int % 2 == 0:
+
+        seq_int = int(seq_int / 2)
+    elif seq_int % 2:
+
+        seq_int = seq_int * 3 + 1
+    else:
+
+        print("something wrong if we get here")
+
+    # avoid trailing comma when we hit 1
+    if seq_int == 1:
+
+        print('{0:5d} '.format(seq_int))
+    else:
+
+        print('{0:5d}, '.format(seq_int), end='')
+
+    count += 1
+    if count % 10 == 0:
+
+        print()
+
+else:
+
+    print(count,"elements in sequence starting at",start_int)
+
+    
+print('The end')
---
+++ spam.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+print(2 ** 8)                              # Raise to a power.
+print('the bright side ' + 'of life')      # + means concatenation.
